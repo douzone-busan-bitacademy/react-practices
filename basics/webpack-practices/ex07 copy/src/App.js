6a267eafdec95f872196f1c9db7fa596f22be9ab
douzone-busan-bitacademy <douzone.busan.bitacademy@gmail.com>
2021-07-22 15:39:26 +0900

    .

--- basics/webpack-practices/ex07 copy/src/App.js
+++
@@ -1,9 +0,0 @@
-import content from './hello.txt';
-
-const App = function(){
-    const app = document.createElement('h1');
-    app.textContent = content.text;
-    return app;
-}
-
-export { App };
--- basics/webpack-practices/ex07 copy/src/text-loader.js
+++
@@ -1,4 +0,0 @@
-module.exports = function(source) {
-    console.log('text-loader preprocessing...');
-    return `module.exports = {text: '${source}'}`;
-}
