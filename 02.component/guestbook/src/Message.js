dd82cf6436bf7501f60fedad49a61a45d96931c5
douzone-busan-bitacademy <douzone.busan.bitacademy@gmail.com>
2021-07-29 15:18:20 +0900

    guestbook#2

--- 02.component/guestbook/src/Message.js
+++ 02.component/guestbook/src/Message.js
@@ -1,17 +1,18 @@
 import React, {Fragment} from 'react';
+import styles from './assets/scss/Message.scss';
 
-export default function Message({ name, message }) {
+export default function Message({ no, name, message }) {
     return (
-        <li className="Guestbook__List__Item">
+        <li className={ styles.Message__List__Item }>
             <strong>{ name }</strong>
             <p>
                 { message && message.split('\n').map((line, index) => index > 0 ?
-                    <Fragment>
+                    <span key={`${no}-${index}`}>
                         <br/>
                         { line }
-                    </Fragment> : line) }
+                    </span> : line) }
             </p>
-            <strong></strong>
+            <strong/>
             <a href=''>삭제</a>
         </li>
     );
