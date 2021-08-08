dc8b67a45bc28e61b1065027f40dffff2779323a
douzone-busan-bitacademy <83158968+douzone-busan-bitacademy@users.noreply.github.com>
2021-08-09 08:47:45 +0900

    Add files via upload
    과제 풀이: Clock Component

---
+++ 02.component/ex05/src/04/App.js
@@ -0,0 +1,39 @@
+import React, {useState, useEffect} from 'react';
+import './assets/scss/App.scss'
+import Clock from './Clock';
+
+export default function App() {
+    const [state, setState] = useState({
+        count: 0,
+        hours: '00',
+        minutes: '00',
+        seconds: '00'
+    });
+
+    console.log(state);
+
+    useEffect(() => {
+        setTimeout(function () {
+            const date = new Date();
+            const hours = date.getHours();
+            const minutes = ('0' + date.getMinutes()).slice(-2);
+            const seconds = ('0' + date.getSeconds()).slice(-2);
+
+            setState({
+                hours: ('0' + (hours == 0 ? 12 : (hours > 12 ? hours - 12 : hours))).slice(-2),
+                minutes: minutes,
+                seconds: seconds,
+                count: state.count === 10 ? 0 : state.count + 1
+            })
+        }, 1000);
+    });
+
+    return ( state.count === 100000 ?
+                null :
+                <Clock
+                    message={'ex05: useEffect Hook example'}
+                    hours={state.hours}
+                    minutes={state.minutes}
+                    seconds={state.seconds} />
+    );
+}
